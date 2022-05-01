ac50a5129c7c03f8b3d2d7256505212fdddfb320
dsoldera <dsoldera@gmail.com>
2022-05-01 20:04:53 -0300

    updates

--- purchases/src/http/test/test.controller.ts
+++ purchases/src/http/test/test.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PrismaService } from '../../database/prisma/prisma.service';
 import { AuthorizationGuard } from '../auth/authorization.guard';
+import { useUser } from '@auth0/nextjs-auth0';
 
 @Controller('test')
 export class TestController {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  @UseGuards(AuthorizationGuard)
+  // @UseGuards(AuthorizationGuard)
   hello() {
-    return this.prisma.customer.findMany();
+    const { user } = useUser();
+    {
+      JSON.stringify(user, null, 2);
+    }
+    // return this.prisma.customer.findMany();
   }
 }
